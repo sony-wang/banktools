8192b9dd73ddbb1fe476b1e741420974d0bb30a2
sony <phenombox@gmail.com>
2020-02-19 16:51:01 +0800

    no message

---
+++ src/components/Tools.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    
+    <h3>{{ title }}</h3>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tools",
+  props: {
+    title:String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "~@/assets/css/style.scss";
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import 'bootstrap'; // Import js file
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import css file
 Vue.config.productionTip = false;
 
 new Vue({
